8940b5c01b05c3e750c947196310fbab020653fa
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2024-09-16 14:04:57 -0300

    implementando funcionalidade de busca

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(candidatos)
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+    let resultados = ""
+
+    // Para cada candidato dentro da lista de dados
+    for (let candidato of candidatos) {
+        resultados += `
+        <div class="item-resultado">
+        <h2>
+            <a href= ${candidato.link} target="blank">${candidato.nome}, ${candidato.partido} </a>
+        </h2>
+        <p class="descricao-meta">${candidato.descricao}</p>
+        </div>
+    `
+    }
+
+    section.innerHTML = resultados
+}
+
+
+
+
+
